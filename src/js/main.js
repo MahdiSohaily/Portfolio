1dfe8e6c38c3ec97e3e2970375482d23a7ec86cc
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-06 00:44:45 +0430

    Update double quotes to single

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,30 @@
-const header = document.querySelector("header");
+const header = document.querySelector('header');
 
-window.addEventListener("scroll", () => {
+window.addEventListener('scroll', () => {
   if (window.scrollY > 600) {
-    header.style.boxShadow = "0 1px 4px  hsl(218, 13%, 48%)";
+    header.style.boxShadow = '0 1px 4px  hsl(218, 13%, 48%)';
   } else {
-    header.style.boxShadow = "none";
+    header.style.boxShadow = 'none';
   }
 });
 
-const navIcon = document.querySelector(".mobile-menu");
-const mainNav = document.querySelector(".main-nav");
+const navIcon = document.querySelector('.mobile-menu');
+const mainNav = document.querySelector('.main-nav');
 
-navIcon.addEventListener("click", function () {
-  mainNav.style.height = "100%";
-  mainNav.classList.toggle("open");
-  if (mainNav.classList.contains("open")) {
-    navIcon.src = "./src/images/close-icon.svg";
+navIcon.addEventListener('click', function () {
+  mainNav.style.height = '100%';
+  mainNav.classList.toggle('open');
+  if (mainNav.classList.contains('open')) {
+    navIcon.src = './src/images/close-icon.svg';
   } else {
-    navIcon.src = "./src/images/menu-icon.svg";
+    navIcon.src = './src/images/menu-icon.svg';
   }
 });
 
-mainNav.addEventListener("click", function () {
-  mainNav.classList.remove("open");
+mainNav.addEventListener('click', function () {
+  mainNav.classList.remove('open');
 });
 
-window.addEventListener("resize", () => {
-  mainNav.classList.remove("open");
+window.addEventListener('resize', () => {
+  mainNav.classList.remove('open');
 });
