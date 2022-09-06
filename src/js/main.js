28e2e77214af3cb38050e200d8eb0e9eb915c03b
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-06 20:18:23 +0430

    Add JS Script to take the cards object

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,66 @@
-const header = document.querySelector('header');
-window.addEventListener('scroll', () => {
+const header = document.querySelector("header");
+window.addEventListener("scroll", () => {
   if (window.scrollY > 600) {
-    header.style.boxShadow = '0 1px 4px  hsl(218, 13%, 48%)';
+    header.style.boxShadow = "0 1px 4px  hsl(218, 13%, 48%)";
   } else {
-    header.style.boxShadow = 'none';
+    header.style.boxShadow = "none";
   }
 });
 
-const navIcon = document.querySelector('.mobile-menu');
-const mainNav = document.querySelector('.main-nav');
+const navIcon = document.querySelector(".mobile-menu");
+const mainNav = document.querySelector(".main-nav");
 
-navIcon.addEventListener('click', () => {
-  mainNav.style.height = '100%';
-  mainNav.classList.toggle('open');
-  if (mainNav.classList.contains('open')) {
-    navIcon.src = './src/images/close-icon.svg';
+navIcon.addEventListener("click", () => {
+  mainNav.style.height = "100%";
+  mainNav.classList.toggle("open");
+  if (mainNav.classList.contains("open")) {
+    navIcon.src = "./src/images/close-icon.svg";
   } else {
-    navIcon.src = './src/images/menu-icon.svg';
+    navIcon.src = "./src/images/menu-icon.svg";
   }
 });
 
-mainNav.addEventListener('click', () => {
-  mainNav.classList.remove('open');
-  navIcon.src = './src/images/menu-icon.svg';
+mainNav.addEventListener("click", () => {
+  mainNav.classList.remove("open");
+  navIcon.src = "./src/images/menu-icon.svg";
 });
 
-window.addEventListener('resize', () => {
-  mainNav.classList.remove('open');
+window.addEventListener("resize", () => {
+  mainNav.classList.remove("open");
 });
+
+// Modal Script
+
+const cards = document.querySelectorAll(".card");
+const cardProp = [];
+
+function cardObject(
+  title,
+  timeline,
+  picture,
+  description,
+  technology,
+  liveURL,
+  sourceURL
+) {
+  this.title = title;
+  this.timeline = timeline;
+  this.picture = picture;
+  this.description = description;
+  this.technology = technology;
+  this.liveURL = liveURL;
+  this.sourceURL = sourceURL;
+}
+
+for (let index = 0; index < cards.length; index++) {
+  let title = cards[index].querySelector(".secondary-header").innerText;
+  let timeline = cards[index].querySelector(".counter").innerHTML;
+  let picture = cards[index].querySelector(".img-container img").src;
+  let description = cards[index].querySelector(".details").innerText;
+  let technology = cards[index].querySelector(".tags").innerHTML;
+  let liveURL = 'https://mahdisohaily.github.io/Portfolio/';
+  let sourceURL= 'https://github.com/MahdiSohaily/Portfolio';
+
+  cardProp.push(new cardObject(title, timeline, picture, description, technology, liveURL, sourceURL));
+}
+
